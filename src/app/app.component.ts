2195b4fd4bfe69f523c3e4a328f53bc7801930a2
Einar Ólafsson <einsiol@gmail.com>
2017-12-15 18:44:19 +0100

    Form add items and remove them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,44 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  personForm: any;
+  people = [];
+
+  constructor(private fb: FormBuilder){
+    this.personForm = this.fb.group({
+      'addPerson': ['', Validators.required],
+      'superPower': [false],
+      'rich':[false],
+      'genius':[false]
+    })
+  }
+
+  savePerson() {
+    let value = this.personForm.value;
+    
+    if (this.personForm.valid) {
+      let person = {
+        name: value.addPerson,
+        superPower: value.superPower,
+        rich: value.rich,
+        genius: value.genius
+      }
+
+      this.people.push(person);
+      console.log(this.personForm.value.addPerson)
+      this.personForm.reset();
+
+    }
+  }
+
+  removePerson(person){
+    let index = this.people.indexOf(person);
+    this.people.splice(index, 1);
+  }
+
+  onChange(event,person,selector) {
+    person[selector] = event.returnValue;
+  }
 }
