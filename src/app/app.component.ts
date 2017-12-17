fbd9637bed7d0a5f5bb831f23b46db9299c61448
Einar Ólafsson <einsiol@gmail.com>
2017-12-18 00:21:29 +0100

    Add filter process and fix checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,71 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common'
+import * as _ from 'lodash';
 interface Person {
   name: string, 
   superPower: boolean, 
   rich: boolean, 
   genius:boolean,
+  delete:boolean,
 }
+
+function compare(prop) {
+	return function (a, b) {
+      // Use toUpperCase() to ignore character casing
+      const genreA = a[prop].toUpperCase();
+      const genreB = b[prop].toUpperCase();
+
+      let comparison = 0;
+      if (genreA > genreB) {
+        comparison = 1;
+      } else if (genreA < genreB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+}
+
+function findClassName(className, items){
+  let found = false;
+  items.forEach(function(item){
+    if(item.className == className) { found = true; } 
+    })
+
+  return found;
+}
+
+function localStorage(key: string, value?: string): Array<Person> {
+  let storage = window.localStorage;
+
+  console.log('save or get')
+
+  if(value){
+    return storage.setItem(key, value)
+  } else {
+    return JSON.parse(storage.getItem(key)) || [];
+  }
+
+}
+
+function getDocument(){
+  return document;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
+  host: {
+    '(document:click)': 'onOutsideClick($event)',
+  },
 })
 
 export class AppComponent {
   personForm: any;
-  people: Array<Person> = [];
+
+  people: Array<Person> = localStorage('people_list');
+
   talents: Array<[string, string]> = [
     ['superPower', 'Super Power'], 
     ['rich', 'Rich'], 
@@ -35,7 +84,16 @@ export class AppComponent {
   filterPeople: Array<Object> = [];
   filterValue: string = null;
 
-  constructor(private fb: FormBuilder, private location: Location, provide: LocationStrategy,){
+  arrangeValues: Object = {
+    'name': null,
+    'superPower': null,
+    'rich': null,
+    'genius':null,
+  }
+
+  document = getDocument();
+
+  constructor(private fb: FormBuilder){
     this.personForm = this.fb.group({
       'addPerson': ['', Validators.required],
       'superPower': [false],
@@ -44,6 +102,10 @@ export class AppComponent {
     })
 
     this.filterValue = window.location.hash.substring(1);
+
+    this.filterArray(this.filterValue);
+    this.getTalent();
+
   }
 
   savePerson() {
@@ -52,15 +114,17 @@ export class AppComponent {
     if (this.personForm.valid) {
       let person: Person = {
         name: value.addPerson,
-        superPower: value.superPower,
-        rich: value.rich,
-        genius: value.genius
+        superPower: value.superPower || false,
+        rich: value.rich || false,
+        genius: value.genius || false,
+        delete: false,
       }
 
       this.people.push(person);
       this.filterArray(this.filterValue);
       this.personForm.reset();
       this.getTalent();
+      localStorage('people_list', JSON.stringify(this.people));
     }
   }
 
@@ -69,6 +133,7 @@ export class AppComponent {
     this.people.splice(index, 1);
     this.filterArray(this.filterValue);
     this.getTalent();
+    localStorage('people_list', JSON.stringify(this.people));
   }
 
   onChange(event,person,selector) {
@@ -76,6 +141,7 @@ export class AppComponent {
     let value = event.target.checked;
     person[selector] = value;
     this.getTalent();
+    localStorage('people_list', JSON.stringify(this.people));
   }
 
   getTalent() {
@@ -100,7 +166,32 @@ export class AppComponent {
       this.filterPeople = [...this.people];
     } else {
       this.filterPeople = this.people.filter(person => person[value] === true);
-      this.filterValue = value;
+    }
+    this.filterValue = value;
+  }
+
+
+  arrange(value){
+    let order =  this.arrangeValues[value] ? 'desc' : 'asc';
+    console.log(this.arrangeValues[value]);
+    this.filterPeople = _.orderBy(this.filterPeople, [value], [order]);
+    this.arrangeValues[value] = !this.arrangeValues[value];
+    console.log(this.arrangeValues[value]);
+    
+    
+  }
+
+  onOutsideClick(event){
+    if(event.path){
+      if(!findClassName('delete', event.path)) {
+        this.filterPeople.forEach(function(person: Person){
+          person.delete = false;
+        })
+      }
     }
   }
 }
+
+
+// Set the reverse order of the sorting function: _.reverse(shortBy...);
+// Set arrows and values to store what order has been set in the table header
